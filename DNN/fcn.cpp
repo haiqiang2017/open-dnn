532b6f491993d9ab55cde5ec7bad5822c8ce6a10
haiqiang <Venus_haiqiang@163.com>
2019-05-12 18:21:22 +0800

    fcn done

--- DNN/fcn.cpp
+++ DNN/fcn.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
 	vector<Vec3b> labels = readColors();
 	Mat blobimg = blobFromImage(src);
 
-	//init net
+
 	Net net = readNetFromCaffe(fcn_txt,fcn_model);
 	if (net.empty())
 	{
@@ -33,16 +33,58 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	//use net
-	float time = getTickCount();
+	float time = getTickCount(); //计时器
 	cout << time;
 	net.setInput(blobimg, "data");
 	Mat score = net.forward("score");
 	float tt = getTickCount() - time;
 	cout << "time consume: " << (tt / getTickFrequency() * 1000) << endl;
 
+
+	cout << score.size <<" "<< score.size[1]<<" " << score.size[2]<<" " << score.size[3] << endl;;
 	const int rows = score.size[2];//height
 	const int cols = score.size[3];//width
+	const int chls = score.size[1];//channels
+
+	Mat maxC1(rows, cols, CV_8UC1); //存储最终选定的通道（21分之一）
+	Mat MaxVal(rows, cols, CV_32FC1);//存储label的key
+
+	//我们要遍历的score 是数据结构为 21个通道，每个通道是500*500像素的结果
+	//set 查找表
+	for (int c = 0; c < chls; c++) //一共21个通道代表21个通道在每个下像素点的置信度
+	{
+		for (int row = 0; row < rows; row++)//500行
+		{
+			const float *ptrScore = score.ptr<float>(0, c, row);//指向每个通道的第1个位置，存储的是21个label的置信度 
+		//	cout << *ptrScore << endl;
+			uchar *ptrMaxC1 = maxC1.ptr<uchar>(row); //为了
+			float *ptrMaxVal = MaxVal.ptr<float>(row);
+			for(int col = 0; col < cols; col++) //500列
+			{
+				if (ptrScore[col] > ptrMaxVal[col])
+				{
+					ptrMaxVal[col] = ptrScore[col]; //存储最终的值也就是color帐的结果
+					ptrMaxC1[col] = (uchar)c;//存储最终选择的通道
+				}
+			}
+		}
+	}
+
+	//look up colors
+	Mat result = Mat::zeros(rows, cols, CV_8UC3);//初始化全0的图像矩阵
+	for (int row = 0; row < rows; row++)
+	{
+		const uchar *ptrMaxC1 = maxC1.ptr<uchar>(row); //指向对应的查找表中的行
+		Vec3b *ptrColor = result.ptr<Vec3b>(row);//指向查找表中的行
+		for (int col = 0; col < cols; col++)
+		{
+			ptrColor[col] = labels[ptrMaxC1[col]];//对应位置
+		}
+	}
+	imshow("result", result);
+	Mat dst;
+	addWeighted(src, 0.4, result, 0.6, 0, dst);
+	imshow("FCN", dst);
 
 	waitKey(0);
 	return 0;
